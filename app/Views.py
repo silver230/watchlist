bc06c7c7649e78ae6f799e7dc8b4abac0ba5aaff
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-05 17:03:37 +0300

    We first import the get_movies() function from the request module.

--- app/Views.py
+++ app/Views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_movies
 
 # Views
 @app.route('/')
@@ -23,8 +24,11 @@ def index ():
     '''
     view root page function that returns the index page and its data
     '''
-    title = 'Home - welcome to The best Movie Review Website Online'  
-    return render_template('index.html', title = title)  
+    popular_movies = get_movies('popular')
+    print(popular_movies)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title,popular = popular_movies)
+    
 
 def movie():
 
