6bef0c47d33912d5cc848c3722b1b2605757ebd5
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-05 16:58:16 +0300

    created  a function process_results() that takes in a list of dictionaries.then create an empty list movie_results this is where we will store our newly created movie objects.

--- app/request.py
+++ app/request.py
@@ -30,5 +30,31 @@ def get_movies(category):
 
     return movie_results
 
+def process_results(movie_list):
+    '''
+    Function  that processes the movie result and transform them to a list of Objects
+
+    Args:
+        movie_list: A list of dictionaries that contain movie details
+
+    Returns :
+        movie_results: A list of movie objects
+    '''
+    movie_results = []
+    for movie_item in movie_list:
+        id = movie_item.get('id')
+        title = movie_item.get('original_title')
+        overview = movie_item.get('overview')
+        poster = movie_item.get('poster_path')
+        vote_average = movie_item.get('vote_average')
+        vote_count = movie_item.get('vote_count')
+
+        if poster:
+            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+            movie_results.append(movie_object)
+
+    return movie_results
+
+
 def get_movies(category):
     pass
