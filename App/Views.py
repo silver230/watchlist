d7ac1af63cdb6a5d9ba5c570c5e8e406853b1bf4
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-05 11:15:42 +0300

    rendered a template on the view.py page to  show the message

---
+++ App/Views.py
@@ -0,0 +1,10 @@
+# Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    message = 'Hello World'
+    return render_template('index.html',message = message)
