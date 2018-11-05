e510203a3feb41eb7f7c97f9345d4e25ecbdacbf
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-05 14:48:46 +0300

    We add HTML structure to our template. Inside our <head></head> we have a control block with an if statement.

--- app/Views.py
+++ app/Views.py
@@ -8,7 +8,8 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    message = 'Hello World'
+    message = 'Hello World '
+    # message = 'how about you'
     return render_template('index.html',message = message)
 
 @app.route('/movie/<int:movie_id>')
@@ -25,3 +26,11 @@ def index ():
     title = 'Home - welcome to The best Movie Review Website Online'  
     return render_template('index.html', title = title)  
 
+def movie():
+
+    '''
+    View root page function that returns the movie page and its data
+    '''
+
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('movie.html', title = title)
