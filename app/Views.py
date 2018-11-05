f8684c7415a4a2a9959d8971870849b9dc65f6d2
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-05 14:24:54 +0300

    renamed configr to config

--- app/Views.py
+++ app/Views.py
@@ -2,6 +2,15 @@ from flask import render_template
 from app import app
 
 # Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    message = 'Hello World'
+    return render_template('index.html',message = message)
+
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
 
