2b48dd66df07e2ad7e874142bd70ecc011f12537
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-05 14:31:20 +0300

    i want to pass in a title to our templates instead of defining them inside the html structure. We create a variable title and pass it into our templates.

--- app/Views.py
+++ app/Views.py
@@ -18,4 +18,10 @@ def movie(movie_id):
     View movie page function that returns the movie details page and its data
     '''
     return render_template('movie.html',id = movie_id)
+def index ():
+    '''
+    view root page function that returns the index page and its data
+    '''
+    title = 'Home - welcome to The best Movie Review Website Online'  
+    return render_template('index.html', title = title)  
 
