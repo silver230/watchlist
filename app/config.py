7d8741b968c42cd8dac70077fa2bf84142ed48c9
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-05 16:05:21 +0300

    added Api key

--- app/config.py
+++ app/config.py
@@ -25,3 +25,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+class Config:
+    '''
+    General configuration parent class
+    '''
+    MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/550?api_key=df68aabef207489b3d32af3fd67f93bf'
